dc713a21b849ac63776fa54355a216b65a986004
yeongi <cbg5255@gmail.com>
2023-05-14 19:31:36 +0900

    chore : project init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from "./App";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
+  document.getElementById("root"),
 );
