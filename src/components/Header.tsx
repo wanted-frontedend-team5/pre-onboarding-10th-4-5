79f5dccba2fd875fa3c8e28f6a17bf4567daeadc
KKI147 <111959118+KKI147@users.noreply.github.com>
2023-05-16 17:07:14 +0900

    feat : layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,7 @@
-import { CSSProperties } from 'react';
-
 const Header = () => {
-  const headerStyle: CSSProperties = {
-    padding: '20px 0',
-    lineHeight: '1.5em',
-  };
-  const titleStyle: CSSProperties = {
-    fontSize: '6rem',
-    fontWeight: '600',
-    marginBottom: '2rem',
-    lineHeight: '1em',
-    color: '#ececec',
-    textAlign: 'center',
-  };
-
   return (
-    <header style={headerStyle}>
-      <h1 style={titleStyle}>Toodos</h1>
+    <header>
+      <h1>Toodos</h1>
     </header>
   );
 };
